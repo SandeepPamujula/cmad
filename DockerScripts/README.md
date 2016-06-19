08fde627f23468863f071263cff5845c24fe034b
Sandeep Kumar Pamujula <sandeek5@cisco.com>
2016-06-19 12:14:32 +0530

    Docker file added

---
+++ DockerScripts/README.md
@@ -0,0 +1,1 @@
+# Docker scripts for Demo
---
+++ DockerScripts/buildDocker.sh
@@ -0,0 +1,32 @@
+echo  "Start -------------------------------->"
+
+docker rm -f loginms
+docker rmi loginms 
+
+docker rm -f haproxylb 
+docker rmi haproxylb 
+
+docker rm -f blogms 
+docker rmi blogms 
+
+rm -r demo2
+mkdir demo2
+cd demo2
+git clone https://github.com/SandeepPamujula/BlogAppMicroServices.git
+
+echo  "$$$ Create Login ms docker container $$$"
+cd BlogAppMicroServices/LoginMicroService
+mvn clean install
+docker build --tag loginms .
+
+echo  "$$$ Create Blog ms docker container $$$"
+cd BlogAppMicroServices/BlogMicroService
+mvn clean install
+docker build --tag blogms .
+
+echo  "$$$ Create haproxy docker container $$$"
+cd /root/demo2/BlogAppMicroServices/blogapp_haproxy
+docker build --tag haproxylb .
+
+
+echo  "End -------------------------------->"
