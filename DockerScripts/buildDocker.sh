4db04c0380face8e625e5ee95d08ddd90a9fe5e3
Sandeep Kumar Pamujula <sandeek5@cisco.com>
2016-06-21 11:35:07 +0530

    cluster.xml copy removed

--- DockerScripts/buildDocker.sh
+++ DockerScripts/buildDocker.sh
@@ -17,13 +17,13 @@ git clone https://github.com/SandeepPamujula/BlogAppMicroServices.git
 echo  "$$$ Create Login ms docker container $$$"
 cd BlogAppMicroServices/LoginMicroService
 mvn clean install
-cp ../config/cluster_login.xml ./target/classes/cluster.xml
+#cp ../config/cluster_login.xml ./target/classes/cluster.xml
 docker build --tag loginms .
 
 echo  "$$$ Create Blog ms docker container $$$"
 cd ../BlogMicroService
 mvn clean install
-cp ../config/cluster_blog.xml ./target/classes/cluster.xml
+#cp ../config/cluster_blog.xml ./target/classes/cluster.xml
 docker build --tag blogms .
 
 echo  "$$$ Create haproxy docker container $$$"
